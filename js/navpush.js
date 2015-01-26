ed5df49a919b6184450c5809e556982af4d32a6a
André Gonçalves <andreslb1@gmail.com>
2015-01-26 13:17:49 +0100

    touch start jquery fix

--- js/navpush.js
+++ js/navpush.js
@@ -2,20 +2,8 @@
 
 $(document).ready(function() {
 
-	$('body').on('touchstart', function(e) {
-		if ($('body').hasClass('canvas-slid')) {
-			if ($(e.target).hasClass('container')) {
-				e.preventDefault();
-			}
-		}
-	});
-
-	$('body').on('touchmove', function(e) {
-		if ($('body').hasClass('canvas-slid')) {
-			if ($(e.target).hasClass('container')) {
-				e.preventDefault();
-			}
-		}
+	$("#page-container").bind("touchmove", function(e) {
+		e.stopPropagation();
 	});
 })
 
