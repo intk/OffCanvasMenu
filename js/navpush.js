9804be0a957761f709279aa4325457712af51761
André Gonçalves <andreslb1@gmail.com>
2015-01-26 13:06:53 +0100

    touch start jquery fix

--- js/navpush.js
+++ js/navpush.js
@@ -2,12 +2,6 @@
 
 $(document).ready(function() {
 
-	$(".navmenu").on("swipe", function(e){
-		if ($('body').hasClass('canvas-slid')) {
-			e.preventDefault();
-		}
-	});
-
 	$('.container').on('touchstart', function(e) {
 		if ($('body').hasClass('canvas-slid')) {
 			e.preventDefault();
